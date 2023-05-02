4345020f931e7cb1370a0c497a57fe0096168d26
Gotfryd3 <102725179+Gotfryd3@users.noreply.github.com>
2023-05-03 00:02:19 +0200

    lekkie_poprawki_pod_naciskiem_adriana

--- projekt_grupowy_rscript.R
+++ projekt_grupowy_rscript.R
@@ -9,6 +9,10 @@
 ##  Libraries
 #
 library(dplyr)
+library(tidyverse)
+library(caTools)
+library(caret)
+library(ROCR)
 
 #####
 ##  Loading data
@@ -16,16 +20,11 @@ library(dplyr)
 filename <- "ObesityDataSet_raw_and_data_sinthetic.csv"
 rawData <- read.csv(filename)
 
-#####
-##  Type definition of variables
-#
-rawDataset <- cbind(as.data.frame(lapply(rawData[c("Gender", "family_history_with_overweight", "FAVC", "CAEC", "SMOKE", "SCC", "CALC", "MTRANS", "NObeyesdad")], as.factor)), 
-                    as.data.frame(lapply(rawData[c("Age", "Height", "Weight", "FCVC", "NCP", "CH2O", "FAF", "TUE")], as.numeric)))
 
 #####
 ##  Data description
 #
-summary(rawDataset)
+summary(rawData)
 
 # FCVC - frequency of consumption of vegetables
 # CAEC - consumption of food between meals
@@ -42,5 +41,46 @@ summary(rawDataset)
 #####
 ##  Preparing data to logistic regression
 #
-preparedData <- rawDataset[-9]
+preparedData <- subset(rawData, select = -c(Gender, NObeyesdad, family_history_with_overweight, SMOKE, CALC, CAEC, MTRANS, SCC, FAVC))
+preparedData["Proper_weight"] <-  if_else(rawData$NObeyesdad == "Normal_Weight", 1, 0, missing = 0)
+preparedData["Male"] <-  if_else(rawData$Gender == "Male", 1, 0, missing = 0)
+preparedData["FHWO"] <-  if_else(rawData$family_history_with_overweight == "yes", 1, 0, missing = 0)
+preparedData["CCM"] <-  if_else(rawData$SCC == "yes", 1, 0, missing = 0)
+preparedData["FOCHCF"] <-  if_else(rawData$FAVC == "yes", 1, 0, missing = 0)
+preparedData["CIGARETES"] <-  if_else(rawData$SMOKE == "yes", 1, 0, missing = 0)
+preparedData["COA"] <-  if_else(rawData$CALC == "Frequently", 2, if_else(rawData$CALC == "Sometimes", 1, 0, missing = 0), missing = 0)
+preparedData["COFBM"] <-  if_else(rawData$CAEC == "Always", 4, if_else(rawData$CAEC == "Frequently", 3, if_else(rawData$CAEC == "Sometimes", 2, 1, missing = 1), missing = 1), missing = 1)
+preparedData["COMMUTE"] <-  if_else(rawData$MTRANS == "Automobile", 4, if_else(rawData$MTRANS == "Motorbike", 3, if_else(rawData$MTRANS == "Public_Transportation", 2, 1, missing = 1), missing = 1), missing = 1)
 
+#####
+##  Type definition of variables
+#
+dataset <- as.data.frame(lapply(preparedData[c("Age", "Height", "Weight", "FCVC", "NCP", "CH2O", "FAF", "TUE", "Proper_weight", "Male", "FHWO", "CCM", "FOCHCF", "CIGARETES", "COA", "COFBM", "COMMUTE")], as.numeric))
+summary(dataset)
+
+
+#####
+##  Data partition
+#
+logicClassTrue <- data.frame(filter(dataset, Proper_weight == 1))
+logicClassFalse <- data.frame(filter(dataset, Proper_weight == 0))
+
+split = sample.split(logicClassTrue, SplitRatio = 0.7)
+trainDataT = subset(logicClassTrue, split == TRUE)
+testDataT  = subset(logicClassTrue, split == FALSE)
+
+split = sample.split(logicClassFalse, SplitRatio = 0.7)
+trainDataF = subset(logicClassFalse, split == TRUE)
+testDataF  = subset(logicClassFalse, split == FALSE)
+
+trainData <- rbind(trainDataT, trainDataF)
+testData <- rbind(testDataT, testDataF)
+
+#####
+##  First model
+#
+formula <- Proper_weight ~ Age + Height + Weight + FCVC + NCP + CH2O + FAF + TUE + Male + FHWO + CCM + FOCHCF + CIGARETES + COA + COFBM + COMMUTE
+logistic <- glm(formula, family=binomial(link="logit"), as.data.frame(trainData))
+summary(logistic)
+pred <- predict(logistic, testData)
+t <- table(testData$Proper_weight, pred) 
